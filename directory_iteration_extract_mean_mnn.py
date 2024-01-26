3caa8e047d599a13842adfd69e16c19404c9c267
Tom Collins <tomthecollins@gmail.com>
2024-01-26 14:24:01 -0500

    Tidy up.

--- directory_iteration_extract_mean_mnn.py
+++ directory_iteration_extract_mean_mnn.py
@@ -19,7 +19,7 @@ main_paths = {
     "out_dir": os.path.join(
       "/home", "txc970", "repos", "mmi-mus-ai-python", "out"
     ),
-    "out_file_name": "mean_mnns.txt"
+    "out_file_name": "mean_mnns"
   },
   "another_user": {
     # ...
